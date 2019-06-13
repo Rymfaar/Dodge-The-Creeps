69e56eca7be320cce0461516548eb1fb0301f1a3
abeaudoin <alexis.beaudoin06@gmail.com>
2019-06-13 12:15:10 +0200

    [ADD] New feature: random mob scale

--- Mob.gd
+++ Mob.gd
@@ -2,10 +2,16 @@ extends RigidBody2D
 
 export var MIN_SPEED = 150 # Minimum speed range
 export var MAX_SPEED = 250 # Maximum speed range
+export var MAX_SCALE = .75 # Maximum scale
+export var MIN_SCALE = .55 # Minimum scale
 var mob_types = ["Flyer", "Swimmer", "Walker"]
 
-# When a prefab/node is created and enters the scene tree 
+# When a prefab/node is created and enters the scene tree
 func _ready():
+	$AnimatedSprite.scale.x = rand_range(MIN_SCALE, MAX_SCALE)
+	$AnimatedSprite.scale.y = $AnimatedSprite.scale.x
+	$CollisionShape2D.scale.x = $AnimatedSprite.scale.x
+	$CollisionShape2D.scale.y = $AnimatedSprite.scale.x
 	$AnimatedSprite.animation = mob_types[randi() % mob_types.size()]
 	$AnimatedSprite.play()
 	$Tween.interpolate_property(self, "scale", self.scale, Vector2(0,0), 0.5,Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
